aad5fb0d6b3be5af8e5e46f68a16a4fa1689dfa0
Siming Yuan <siyuan@cisco.com>
2020-01-07 12:00:53 -0500

    removing ces monitor code

--- src/genie/webdriver/__init__.py
+++ src/genie/webdriver/__init__.py
@@ -5,21 +5,3 @@ __contact__ = ['pyats-support@cisco.com', 'pyats-support-ext@cisco.com']
 
 from .webpage import WebPage
 
-# try to record usage statistics
-#  - only internal cisco users will have stats.CesMonitor module
-#  - below code does nothing for DevNet users -  we DO NOT track usage stats
-#    for PyPI/public/customer users
-try:
-    # new internal cisco-only pkg since devnet release
-    from pyats.cisco.stats import CesMonitor
-except Exception:
-    try:
-        # legacy pyats version, stats was inside utils module
-        from pyats.utils.stats import CesMonitor
-    except Exception:
-        CesMonitor = None
-
-finally:
-    if CesMonitor is not None:
-        # CesMonitor exists -> this is an internal cisco user
-        CesMonitor(action = __name__, application='pyATS Packages').post()
